7480b441effce3956b5db68ecd636bf716014f75
krishivpan <103297164+krishivpan@users.noreply.github.com>
2026-01-10 16:20:19 -0500

    add a navbar to switch between pages

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,10 +1,15 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home.jsx";
 
 
 export default function App() {
   return (
     <>
-      <Home/>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
     </>
   )
 }
